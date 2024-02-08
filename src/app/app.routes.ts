ec71380fb98d24c9ab3d638ad7e9fa2c1f7df29e
Jeptooleonida <104617184+Jeptooleonida@users.noreply.github.com>
2024-02-08 10:33:28 +0300

    initial setup of pages and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { FindStoreComponent } from './pages/find-store/find-store.component';
+import { StoreComponent } from './pages/store/store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+       { path:'', component:HomepageComponent },
+       { path:'blog', component:BlogComponent} ,
+       { path:'categories', component:CategoriesComponent},
+       { path:'collections', component:CollectionsComponent},
+       { path:'find-store', component:FindStoreComponent},
+       { path: 'store', component:StoreComponent},
+         
+    
+];
