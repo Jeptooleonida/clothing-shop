6e85d16786090aaec365b5d3006c343134199abf
Jeptooleonida <104617184+Jeptooleonida@users.noreply.github.com>
2024-02-08 17:07:38 +0300

    fetch items from api endpoints

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+ 
+  httpClient = inject (HttpClient);
+  data:any[] = [];
 
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+    .get('https://fakestoreapi.com/products')
+    
+    .subscribe((data: any) =>  {
+      console.log(data);
+      this.data = data})
+  }
 }
