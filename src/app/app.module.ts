8de0fea7576e1363222d30c79d3d3f04c6e2c284
Jeptooleonida <104617184+Jeptooleonida@users.noreply.github.com>
2024-02-13 11:02:07 +0300

    update the navbar styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-
-
+import { AppComponent } from './app.component'; // Import AppComponent if it's declared
 
 @NgModule({
   declarations: [
@@ -14,13 +13,13 @@ import { ProductListComponent } from './components/product-list/product-list.com
   ],
   imports: [
     CommonModule,
-    AppComponent,
     BrowserModule,
-    ProductListComponent,
+    HttpClientModule,
     RouterModule.forRoot([
-        { path: 'products/:productId', component: ProductDetailsComponent },
-
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
-  ]
+  ],
+  providers: [],
+  bootstrap: [] 
 })
 export class AppModule { }
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from "../../components/product-list/product-list.component";
-
+// import { HttpClientModule } from '@angular/common/http';
 @Component({
     selector: 'app-homepage',
     standalone: true,
     templateUrl: './homepage.component.html',
     styleUrl: './homepage.component.css',
-    imports: [ProductListComponent]
+    imports: [
+        ProductListComponent,
+        // HttpClientModule
+    ]
 })
 export class HomepageComponent {
  
